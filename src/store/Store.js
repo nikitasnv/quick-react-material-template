bae04f0d0b28c3a127aac7ac05e54ceafa4cd22d
nikitasnv <nikitasnv@gmail.com>
2019-01-24 19:54:06 +0300

    -

--- src/store/Store.js
+++ src/store/Store.js
@@ -5,7 +5,8 @@ import rootSaga from './Sagas';
 import appReducer from './Reducers';
 
 export default (history) => {
-	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	const composeEnhancers = (process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+		|| compose;
 	const middleware = createSagaMiddleware();
 	const store = createStore(appReducer(history), composeEnhancers(applyMiddleware(
 		routerMiddleware(history), middleware,
